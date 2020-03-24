198efec177d43302409259753102ef86fb7245b1
GaetanDupont <gaetan.dupont@live.fr>
2020-03-24 11:27:56 +0100

    Add ball.js :rocket: Import gltf :sparkles: Import three :sparkles: Set up the scene :rocket:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,105 @@
 import './style/main.styl'
 import './animationJs/sceneController'
 import './animationJs/fireballAnimation'
+import './style/main.styl'
+import * as THREE from 'three'
+import Ball from './javascript/ball.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+/**
+ * Sizes
+ */
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (_event) =>
+{
+    cursor.x = _event.clientX / sizes.width - 0.5
+    cursor.y = _event.clientY / sizes.height - 0.5
+})
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Lights
+ */
+ const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+  scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+directionalLight.position.x = 5
+directionalLight.position.y = 5
+directionalLight.position.z = 5
+scene.add(directionalLight)
+
+/**
+* ball
+*/
+// const gltfLoader = new GLTFLoader()
+// gltfLoader.load(
+//     '/models/mikasa_ft-5/scene.gltf',
+//     (gltf) =>
+//     {
+//         while(gltf.scene.children.length)
+//         {
+//             const child = gltf.scene.children[0]
+//             scene.add(child)
+//         }
+//     }
+// )
+
+const ball = new Ball()
+scene.add(ball.group)
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 20)
+camera.position.z = 8
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(window.devicePixelRatio)
+document.body.appendChild(renderer.domElement)
+
+/**
+ * Resize
+ */
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+/**
+ * Loop
+ */
+const loop = () =>
+{
+    window.requestAnimationFrame(loop)
+
+    // Render
+    renderer.render(scene, camera)
+}
+
+loop()
---
+++ src/javascript/Ball.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+export default class Ball
+{
+    constructor()
+    {
+        this.group = new THREE.Group()
+
+        const gltfLoader = new GLTFLoader()
+
+        gltfLoader.load(
+            '/models/mikasa_ft-5/scene.gltf',
+            (_gltf) =>
+            {
+                this.ball = _gltf.scene.children[0]
+                console.log(_gltf.scene)
+
+                this.ball.scale.set(1, 1, 1)
+                this.group.add(this.ball)
+            }
+        )
+    }
+}
