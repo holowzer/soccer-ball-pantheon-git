a614503a346e6e2838ffcbe88977a2685dfd1581
holowzer <arthurlasnelhetic@gmail.com>
2020-03-24 11:11:29 +0100

    add move to the landing page

--- src/animationJs/fireballAnimation.js
+++ src/animationJs/fireballAnimation.js
@@ -1,1 +1,16 @@
-console.log('here will be the landing page cursor and overall animations')
+const fireball = document.querySelector('.radialSun')
+const textMove = document.querySelector('.textCursorMove')
+
+let cursorX 
+let cursorY
+
+// get the curosor position 
+window.addEventListener('mousemove',(e)=>{
+     cursorX = e.clientX - 650
+     cursorY = e.clientY - 150
+     console.log(cursorX , cursorY)
+     fireball.style.transform = "translateX("+  cursorX/7 +"px) translateY("+  cursorY/5  +"px)"
+     textMove.style.transform = "rotateY("+  cursorX/125 +"deg) translateY("+  cursorY/60  +"px)"
+})
+
+ 
