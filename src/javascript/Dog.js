79c63ff15c31e85e180929f9ccf963fb2956f7b0
sassanTMAX <andy.chea@hetic.net>
2020-03-26 14:27:12 +0100

    :sparkles: add dog ball model

---
+++ src/javascript/Dog.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+export default class Dog
+{
+    constructor()
+    {
+        this.group = new THREE.Group()
+
+        const dracoLoader = new DRACOLoader()
+        dracoLoader.setDecoderPath('/draco/')
+
+        const gltfLoader = new GLTFLoader()
+        gltfLoader.setDRACOLoader(dracoLoader)
+
+        gltfLoader.load(
+            '/models/dog-ball/scene.gltf',
+            (_gltf) =>
+            {
+                this.dog = _gltf.scene.children[0]
+                this.dog.scale.set(4, 4, 4)
+                this.group.add(this.dog)
+            }
+        )
+    }
+}
